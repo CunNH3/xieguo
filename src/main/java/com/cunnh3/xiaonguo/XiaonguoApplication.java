c91b73a84e8c77579bd8ccb1f5e69452bc15b1b1
cunnh3 <872377689@qq.com>
2021-02-02 22:00:37 +0800

    git init

---
+++ src/main/java/com/cunnh3/xiaonguo/XiaonguoApplication.java
@@ -0,0 +1,13 @@
+package com.cunnh3.xiaonguo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaonguoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaonguoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cunnh3/xiaonguo/XiaonguoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cunnh3.xiaonguo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaonguoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
