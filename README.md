fa49da230344bfecede769dff075f57a1024602e
cunnh3 <872377689@qq.com>
2021-02-04 21:11:21 +0800

    github authon

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ##鲜果社区
 用来吐槽校园生活
-## 技术栈
+## 技术栈 
+- github OK
 - springBoot
 - github
---
+++ src/main/java/com/cunnh3/xiaonguo/controller/AuthorizeController.java
@@ -0,0 +1,40 @@
+package com.cunnh3.xiaonguo.controller;
+
+import com.cunnh3.xiaonguo.dto.AccessTokenDTO;
+import com.cunnh3.xiaonguo.dto.GithubUser;
+import com.cunnh3.xiaonguo.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user);
+
+        return "index";
+    }
+}
